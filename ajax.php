51e7f0b754a7278f3e1f8badb1af8ae2729d6f0c
Jess247 <jesss8692@hotmail.com>
2022-02-02 16:25:50 +0100

    add example

---
+++ ajax.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<title>AJAX</title>
+		<meta name="viewport" content=
+		"width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="lib/css/style.css" />
+		<script type="text/javascript"
+		src="lib/js/script2.js"></script>
+
+	</head>
+	<body>
+		<h1>Geben Sie Ihren Namen ein</h1>
+		<form>
+			Name:
+			<input type="text" id="n" />
+		</form>
+		<hr />
+
+		<button id="request">
+			OK
+		</button>
+		<div id="response"></div>
+
+	</body>
+</html>
+
---
+++ formularauswertung.php
@@ -0,0 +1,4 @@
+<?php 
+echo "Hello <b>". $_GET['n'].
+    "</b>. Nice to see you!";
+?>
